1e615a434f3cb526172a8b6bf0c7ca68ef10357c
Abdushukur <abdulshakurturdimatov@gmail.com>
2022-04-30 16:15:37 +0500

    instaled cors

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const app = express();
 const { PORT } = require("./config");
 const { sequelize } = require("./lib/sequelize");
 const router = require("./Controllers");
+const cors = require('cors');
 
 sequelize.sync({ force: false }).then(() => console.log("OK"));
 
@@ -10,5 +11,6 @@ app.use("/public", express.static("public"));
 
 app.use(express.json());
 app.use(router);
+app.use(cors());
 
 app.listen(PORT, console.log(PORT));
