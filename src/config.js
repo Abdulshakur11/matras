3f6d59635c3f33d2a5970511eb872eee8c2f77b7
Abdushukur <abdulshakurturdimatov@gmail.com>
2022-04-30 10:52:31 +0500

    initial commit

--- src/config.js
+++ src/config.js
@@ -1,9 +1,11 @@
 const PORT = process.env.PORT || 4000
-const sequelizeConnection = 'postgres://postgres:0000@localhost:5432/matras'
+const sequelizeConnection = 'postgres://postgres:0000@localhost:5432/matras';
+const elephantConnection = "postgres://hoovzkvq:tFfpKZEAZWFYfM2W8DkAN9rGrcpy27tQ@arjuna.db.elephantsql.com/hoovzkvq"
 const SECRET_KEY = "MAXFIY_KALIT"
 
 module.exports = {
   PORT,
   sequelizeConnection,
-  SECRET_KEY
+  SECRET_KEY,
+  elephantConnection
 }
--- src/lib/sequelize.js
+++ src/lib/sequelize.js
@@ -1,5 +1,5 @@
 const { Sequelize, DataTypes } = require("sequelize");
-const { sequelizeConnection } = require('../config');
-const sequelize = new Sequelize(sequelizeConnection);
+const { elephantConnection } = require('../config');
+const sequelize = new Sequelize(elephantConnection);
 
 module.exports = { sequelize, DataTypes }
